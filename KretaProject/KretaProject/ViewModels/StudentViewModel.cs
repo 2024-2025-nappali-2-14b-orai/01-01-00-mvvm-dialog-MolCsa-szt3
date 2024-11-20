2cfd2ab33978be1ab018cbff67377112f99e47d7
szt3molcsa <szt3molcsa@vasvari.org>
2024-11-20 13:59:52 +0100

    catchup

---
+++ KretaProject/KretaProject/ViewModels/StudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KretaProject.Models;
+
+namespace KretaProject.ViewModels
+{
+    public partial class StudentViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Student _student=new Student();
+    }
+}
