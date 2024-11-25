444f4154bd37a617e19c203f9ee89036d3ba2082
szt3molcsa <szt3molcsa@vasvari.org>
2024-11-25 13:59:52 +0100

    mild amount of rebranding

---
+++ KretaProject/KretaProject/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace KretaProject.Models
+{
+    
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Wins { get; set; }
+        public int Matches { get; set; }
+
+        public Player(string name, string email, DateTime createDate, int wins, int matches)
+        {
+            Name = name;
+            Email = email;
+            CreationDate = createDate;
+            Wins = wins;
+            Matches = matches;
+        }
+
+        public Player()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            CreationDate = new DateTime();
+            Wins = 0;
+            Matches = 0;
+        }
+    }
+}
---
+++ KretaProject/KretaProject/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KretaProject.Models;
+using System.Collections.ObjectModel;
+
+namespace KretaProject.ViewModels
+{
+    public partial class PlayerViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Player _player=new Player();
+        [ObservableProperty]
+        private ObservableCollection<Player> _players = new ObservableCollection<Player>();
+    }
+
+}
