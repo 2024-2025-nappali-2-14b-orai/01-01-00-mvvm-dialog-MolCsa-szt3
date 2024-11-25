ea427a9b5b28e7f81512c7d514b9b5416d1e83ac
szt3molcsa <szt3molcsa@vasvari.org>
2024-11-25 14:04:13 +0100

    player got ToString override and Winrate

--- KretaProject/KretaProject/Models/Player.cs
+++ KretaProject/KretaProject/Models/Player.cs
@@ -8,6 +8,7 @@
         public DateTime CreationDate { get; set; }
         public int Wins { get; set; }
         public int Matches { get; set; }
+        public double Winrate => Matches == 0? 0:(double)(Wins) / Matches;
 
         public Player(string name, string email, DateTime createDate, int wins, int matches)
         {
@@ -25,6 +26,10 @@
             CreationDate = new DateTime();
             Wins = 0;
             Matches = 0;
+        }
+        public override string ToString()
+        {
+            return $"Username:{Name}; Email:{Email};Account Created:{CreationDate.ToShortDateString};Matches:{Matches}; Winrate:{Winrate}";
         }
     }
 }
